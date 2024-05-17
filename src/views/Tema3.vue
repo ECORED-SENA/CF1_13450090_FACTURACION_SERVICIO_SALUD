a00ccd760a71a00d573b9df6df528c68ff24b679
jhonuru <jhon.urueta@misena.edu.co>
2024-05-17 17:00:32 -0500

    Avance 2

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -6,6 +6,218 @@
       .titulo-principal__numero
         span 3
       h1 Tipos de usuarios y planes de beneficios
+    .row.mb-5
+      .col-lg-9.order-2.order-lg-1(data-aos="fade-right")
+        p Desde la creación de la #[b Ley 100 de 1993], los tipos de usuarios del SGSSS se han ido ampliando para garantizar la cobertura a todos los habitantes del país.
+        .row
+          .col-12
+            .p-4.bg-c2
+              .row
+                .col-auto
+                  figure
+                    img(src='@/assets/curso/tema3/img1.svg', style="width:70px" alt='').m-auto
+                .col
+                  p.mb-0 #[b La resolución 5592 de 2015], emitida por el Ministerio de la Protección Social, actualizó integralmente el Plan de Beneficios en Salud con cargo a la Unidad de Pago por Capitación. Esto significa que el Estado colombiano asigna a las Entidades Promotoras de Salud un valor económico por cada persona afiliada, determinado según su ubicación geográfica, edad y sexo, para su administración. Además, los servicios que prestan las IPS deben regirse por los servicios autorizados en el Plan de beneficios para su cobro a las administradoras.
+      .col-lg-3.mb-4.mb-lg-0.order-1.order-lg-2(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema3/img1.png', style="width:290px" alt='').m-auto
+    .row.bg-c11.mb-5
+      .col-12
+        .px-5
+          .row.justify-content-center
+            .col-lg-10(data-aos="fade-right")
+              .titulo-sexto.color-secundario
+                p.mb-0 #[b Tabla 1.] Tipos de usuarios del Sistema General de Seguridad Social en Salud
+              .tabla-a.color-secundario.mb-4
+                table
+                  caption Referencia Tabla - Norma APA
+                  thead
+                    tr
+                      th Condiciones de la normatividad vigente
+                      th Parentesco
+                      th Documento para acreditar afiliación
+                  tbody
+                    tr(style="background-color:#F6F6F6 !important")
+                      td Siempre.
+                      td Cotizante.
+                      td Fotocopia del documento de identidad.
+                    tr(style="background-color:#FFFFFF !important")
+                      td Si están casados.
+                      td Cónyuge.
+                      td
+                        p.mb-0 Fotocopia del documento de identidad.
+                        p.mb-0 Registro civil de matrimonio.
+                    tr(style="background-color:#F6F6F6 !important")
+                      td En unión marital de hecho o unión libre.
+                      td Compañero.
+                      td
+                        p.mb-0 Fotocopia del documento de identidad.
+                        p.mb-0 Declaración juramentada de convivencia vigente (No ante notario). No mayor a seis meses.
+                    tr(style="background-color:#FFFFFF !important")
+                      td Si existen (Los míos, los tuyos y los nuestros) incluye adoptivos con documento legal.
+                      td Hijos entre 0 y 6 años y 364 días.
+                      td Fotocopia del documento de Identidad (Registro civil).
+                    tr(style="background-color:#F6F6F6 !important")
+                      td En el caso de ser el afiliado soltero o de hacer pago adicional de UPC.
+                      td Padres.
+                      td
+                        p.mb-0 Fotocopia del documento de identidad.
+                        p.mb-0 Registros civiles que se requieran para demostrar parentesco.
+                    tr(style="background-color:#FFFFFF !important")
+                      td Caso de cobertura especial - Consultar condiciones.
+                      td Otros hasta tercer grado de consanguinidad. (Abuelos, bisabuelos, tíos, hermanos, sobrinos, nietos, bisnietos).
+                      td
+                        p.mb-0 Fotocopia del documento de Identidad.
+                        p.mb-0 Registros civiles que se requieran para demostrar parentesco.
+                        p.mb-0 Recibo de pago de UPC (planilla de autoliquidación).
+                    tr(style="background-color:#F6F6F6 !important")
+                      td Caso de cobertura especial - Consultar condiciones.
+                      td Nietos que dependan económicamente del abuelo.
+                      td Registro civil para demostrar parentesco.
+    .row.mb-4(data-aos="fade-down")
+      .col-auto 
+        .px-4.bg-c12
+          .row.align-items-end
+            .col-auto.px-0
+              figure.fig-p1
+                img(src='@/assets/curso/tema3/img3.svg', style="width:50px" alt='').m-auto
+            .col
+              h3 Soportes físicos y digitales
+    .row.mb-4
+      .col-lg-9.order-2.order-lg-1(data-aos="fade-right")
+        p El acuerdo de voluntades, parametrizado en cada entidad facturadora, establece que los soportes sean físicos o digitales según el caso, sin dejar de lado lo estipulado en la Resolución 3047 de 2008 y sus adiciones o modificaciones. Además, existe el MAPIPOS (Manual de Actividades, Intervenciones y Procedimientos del Plan Obligatorio de Salud) en el SGSSS, según la Resolución 5261 de 1994, que presenta de manera cefalocaudal (de la cabeza a los pies) las actividades, intervenciones y procedimientos según el plan de beneficios.
+        p.mb-0 Este manual está organizado y clasificado por nivel de atención en el Plan de Beneficios del Sistema, como se ve a continuación:
+      .col-lg-3.mb-4.mb-lg-0.order-1.order-lg-2(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema3/img2.png', style="width:290px" alt='').m-auto
+    .row.mb-5
+      .col-lg-3.d-none.d-lg-block(data-aos="fade-right")
+        figure
+          img(src='@/assets/curso/tema3/img3.png', style="width:290px" alt='').m-auto
+      .col-lg-9(data-aos="fade-left")
+        AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row(titulo="Primer nivel")
+            .col-12
+              p Atención ambulatoria:
+              ul.lista-ul
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Consulta médica general.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Atención inicial, estabilización, resolución o remisión del paciente en urgencias.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Atención odontológica.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Laboratorio clínico.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Radiología.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Medicamentos esenciales.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Citología.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Acciones intra y extramurales de promoción, prevención y control.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Atención quirúrgica.
+              p Servicios con internación:
+              ul.lista-ul
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Atención obstétrica.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Atención n o quirúrgica u obstétrica.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Laboratorio clínico.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Radiología.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Medicamentos esenciales.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Valoración diagnóstica y manejo médico.
+          .row(titulo="Segundo nivel")
+            .col-12
+              p.mb-0 Atención ambulatoria especializada: se define como la atención médica, no quirúrgica, no procedimental, no intervencionista, brindada por un profesional de la medicina, especialista en una o más de las disciplinas de esta ciencia y en la cual ha mediado interconsulta o remisión por el profesional general de la salud. Tendrá el mismo carácter técnico - administrativo para los niveles II y III de complejidad atención; junto con lo del primer nivel y más especializados.
+          .row(titulo="Tercer nivel")
+            .col-12
+              p.mb-0 Condiciones de acceso a los servicios: la utilización de servicios incluidos en este nivel requerirá la aprobación previa de la Entidad Promotora de Salud, quien podrá utilizar para ello comités médicos científicos quienes deberán ceñirse a las guías de atención integral; junto con lo del primer nivel y más especializados.
+          .row(titulo="Cuarto nivel")
+            .col-12
+              p Patologías de tipo catastrófico: son patologías catastróficas aquellas que representan una alta complejidad técnica en su manejo, alto costo, bajo ocurrencia y bajo costo efectividad en su tratamiento. Se consideran dentro de este nivel, los siguientes procesamientos:
+              ul.lista-ul
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Trasplante renal.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Diálisis.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Neurocirugía, sistema nervioso.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Cirugía cardiaca.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Reemplazos articulares.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Manejo del gran quemado.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Manejo del trauma mayor.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Manejo de pacientes infectados por (VIH).
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Quimioterapia y radioterapia para el cáncer.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Manejo de pacientes en unidad de cuidados intensivos.
+                li.d-flex
+                  i.fas.fa-check-circle(style="color:#283DE2")
+                  | Tratamiento quirúrgico de enfermedades congénitas.
+    .row.mb-4(data-aos="fade-down")
+      .col-auto 
+        .px-4.bg-c12
+          .row.align-items-end
+            .col-auto.px-0
+              figure.fig-p1
+                img(src='@/assets/curso/tema3/img3.svg', style="width:50px" alt='').m-auto
+            .col
+              h3 Manuales tarifarios
+    .row.mb-4
+      .col-lg-9.order-2.order-lg-1(data-aos="fade-right")
+        p Existen el Manual Tarifario ISS (Acuerdo 256 de 2001 y Acuerdo 312 de 2004) y el Manual Tarifario SOAT (Decreto 2423 de 1996) como referentes para el cobro de atenciones en la prestación del servicio de salud en Colombia, sin dejar de lado que los contratos que regulan las operaciones comerciales de la prestación del servicio pueden variar según las negociaciones establecidas por las partes contratantes.
+        p.mb-0 Este manual está organizado y clasificado por nivel de atención en el Plan de Beneficios del Sistema, como se ve a continuación:
+      .col-lg-3.mb-4.mb-lg-0.order-1.order-lg-2(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema3/img4.png', style="width:290px" alt='').m-auto
+    .row.justify-content-center.mb-5
+      .col-lg-10(data-aos="fade-right")
+        .row
+          .col-lg-auto.pe-lg-0.mb-4.mb-lg-0
+            figure
+              img(src='@/assets/curso/tema3/img5.png', style="width:250px" alt='').m-auto
+          .col.ps-0
+            .ps-4.ps-lg-0.py-4.pe-4.bg-c7.h-100
+              .row
+                .col-12
+                  .p-4.bg-c8
+                    p.mb-0 EI Manual Tarifario ISS utiliza la Unidad de Valor Relativo (UVR) según puntos establecidos, y se pueden pactar porcentajes adicionales o reducidos de puntos de valor para cada cobro. Por su parte, eI tarifario SOAT cuenta con una tarifa establecida anualmente y se actualiza en enero de cada año, acorde con el incremento del salario mínimo legal vigente en el país.
 
 </template>
 
--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -6,6 +6,225 @@
       .titulo-principal__numero
         span 4
       h1 Acuerdo de voluntades y normatividad
+    .row.mb-5
+      .col-lg-10.order-2.order-lg-1(data-aos="fade-right")
+        p El Estado Colombiano cuenta con diferentes reglas que establecen garantías para la realización de contratos o acuerdos de voluntades entre Instituciones Prestadoras de Salud (IPS) y Empresas Promotoras de Salud (EPS), permitiendo el aseguramiento a los usuarios y la prestación de los servicios de salud correspondientes a cada red de servicios.
+        p.mb-0 Como normatividad principal, se cuentan:
+      .col-lg-2.mb-4.mb-lg-0.order-1.order-lg-2(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema4/img1.svg', style="width:220px" alt='').m-auto
+    .row.mb-5
+      .col-12
+        .p-5.bg-c13(data-aos="fade-down")
+          .tarjeta.tarjeta--azul
+            .p-5
+              SlyderA(tipo="b")
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Resolución 3374 de 2000]
+                    p.mb-0 Reglamenta los datos básicos que deben reportar los prestadores de servicios de salud y las entidades administradoras de planes de beneficios sobre los servicios de salud prestados.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img1.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Decreto 4747 de 2007]
+                    p.mb-0 Regula aspectos de las relaciones entre los prestadores de servicios de salud y las entidades responsables del pago de los servicios de salud.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img2.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Congreso de Colombia, 9 de enero de 2007]
+                    p.mb-0 Regula la relación entre responsables del pago y prestadores de servicios de salud, especialmente en lo relacionado con las EPS en regímenes Contributivo y Subsidiado.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img3.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Resolución 1479 de 2015]
+                    p.mb-0 Señala el procedimiento para el cobro y pago de servicios y tecnologías sin cobertura en el POS para afiliados al régimen subsidiado.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img4.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Resolución 6408 de 2016]
+                    p.mb-0 Modifica el Plan de Beneficios en Salud con cargo a la UPC e incluye anexos técnicos.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img5.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Resolución 6411 de 2016]
+                    p.mb-0 Fija el valor de la UPC para la cobertura del plan de beneficios en salud de los regímenes contributivo y subsidiado en la vigencia 2017.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img6.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Resolución 1132 de 2017]
+                    p.mb-0 Modifica la Resolución 4678 de 2015, adopta la CUPS y dicta otras disposiciones relacionadas con la Resolución 6408 de 2016.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img7.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Resolución 1687 de 2017]
+                    p.mb-0 Sustituye el Anexo 2 "Listado de Procedimientos en Salud del Plan de Beneficios en Salud con cargo a la UPC" de la Resolución 6408 de 2016 y se dictan otras disposiciones.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img8.png', style='width:280px' alt="").m-auto
+    .row.justify-content-center.mb-5
+      .col-lg-10(data-aos="fade-right")
+        .row
+          .col-lg-auto.pe-lg-0.mb-4.mb-lg-0
+            figure
+              img(src='@/assets/curso/tema4/img9.png', style="width:250px" alt='').m-auto
+          .col.ps-0
+            .ps-4.ps-lg-0.py-4.pe-4.bg-c7.h-100
+              .row
+                .col-12
+                  .p-4.bg-c8
+                    p.mb-0 EI facturador desarrolla las competencias para identificar los tipos de acuerdos, para así cobrar los servicios prestados ante las Entidades responsables, según el decreto 4747 de 2007, que tiene en cuenta diferentes tipos de acuerdos de voluntades.
+    .row.mb-5
+      .col-lg-8(data-aos="fade-right")
+        p La relación contractual entre los prestadores y aseguradores se modifica automáticamente, de acuerdo con la dinámica de la actividad regulatoria. Los acuerdos de voluntades se realizan entre los representantes legales de las IPS y las EPS, y tienen que ver con la contratación de los servicios en salud. Para este caso, se contemplan varios aspectos, como pueden ser:
+        .row
+          .col-12
+            .p-2.bg-c14
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Término de duración.
+          .col-12
+            .p-2.bg-c15
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 El valor.
+          .col-12
+            .p-2.bg-c14
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Información de la población objeto.
+          .col-12
+            .p-2.bg-c15
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Servicios contratados.
+          .col-12
+            .p-2.bg-c14
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Mecanismos y forma de pago.
+          .col-12
+            .p-2.bg-c15
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Tarifas que deben ser aplicadas a las unidades de pago.
+          .col-12
+            .p-2.bg-c14
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Proceso y operación del sistema de referencia y contrarreferencia.
+          .col-12
+            .p-2.bg-c15
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Periodicidad en la entrega de información RIPS.
+          .col-12
+            .p-2.bg-c14
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Periodicidad y forma de auditoría.
+          .col-12
+            .p-2.bg-c15
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Mecanismos de interventoría o seguimiento.
+          .col-12
+            .p-2.bg-c14
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Mecanismos para la solución de conflictos.
+          .col-12
+            .p-2.bg-c15
+              .row
+                .col-auto.pe-0
+                  i.fas.fa-check-circle(style="color:#F1C12C")
+                .col
+                  p.mb-0 Mecanismos y términos para la liquidación.
+      .col-lg-4.d-none.d-lg-block(data-aos="fade-left")
+        figure
+          img(src='@/assets/curso/tema4/img10.png', style='width:390px' alt="").m-auto
+    p.mb-5(data-aos="fade-down") El facturador siempre debe tener en cuenta todos los criterios para poder aplicar dicha factura según cada acuerdo, ya que puede haber varias clases de contratos según las diferentes aseguradoras. Los aplicativos que utilizan los responsables de pago están parametrizados con los valores pactados y a cobrar.
+    .row.mb-5
+      .col-12
+        .p-5.bg-c13(data-aos="fade-left")
+          .tarjeta.tarjeta--azul
+            .p-5
+              SlyderA(tipo="b")
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Pago por capitación]
+                    p.mb-0 Es un pago anticipado de una suma fija realizado por persona, la cual tendrá derecho a recibir atención durante un periodo establecido, basado en un conjunto de servicios acordados. La unidad de pago se establece mediante una tarifa previamente convenida, que depende del número de personas que se prevea atender.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img11.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Pago por evento]
+                    p.mb-0 Este mecanismo determina que el pago se realiza por las actividades, procedimientos, intervenciones, insumos y medicamentos proporcionados o suministrados a un paciente durante un periodo concreto, asociado a un evento específico de atención en salud. La unidad de pago la constituye cada actividad, procedimiento, intervención, insumo o medicamento suministrado, con tarifas acordadas con antelación.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img12.png', style='width:280px' alt="").m-auto
+                .row.justify-content-center
+                  .col-lg-7.mb-4.mb-lg-0
+                    p #[b Pago por caso, conjunto integral de atenciones, paquete o grupo relacionado por diagnóstico]
+                    p.mb-0 A través de este mecanismo se efectúa el pago de conjuntos de actividades, procedimientos, intervenciones, insumos y medicamentos, suministrados a un paciente y vinculados a un evento de salud, diagnóstico o grupo de diagnóstico correspondiente. La unidad de pago la conforma cada caso, conjunto o paquete de servicios prestados, o grupo relacionado por diagnóstico, con tarifas previamente pactadas.
+                  .col-lg-4
+                    figure
+                      img(src='@/assets/curso/tema4/img13.png', style='width:280px' alt="").m-auto
+    .row.mb-4(data-aos="fade-down")
+      .col-auto 
+        .px-4.bg-c12
+          .row.align-items-end
+            .col-auto.px-0
+              figure.fig-p1
+                img(src='@/assets/curso/tema3/img3.svg', style="width:50px" alt='').m-auto
+            .col
+              h3 Responsables de pagos Empresas Administradoras de Planes de Beneficios (EAPB)
+    p.mb-5(data-aos="fade-right") Dentro de las EAPB se encuentran las Entidades Promotoras de Salud del Régimen Contributivo y Subsidiado, Empresas Solidarias de Salud, las asociaciones mutuales en sus actividades de salud, las Entidades Promotoras de Salud Indígenas, las Cajas de Compensación Familiar en sus actividades de salud, las entidades que administran planes adicionales de salud, las entidades obligadas a compensar, las entidades adaptadas de salud, las entidades pertenecientes al régimen de excepción de salud y las universidades en sus actividades de salud.
+    .row.justify-content-center
+      .col-lg-10
+        .p-4.bg-c2(data-aos="fade-left")
+          .row
+            .col-md-auto.mb-4.mb-md-0
+              figure
+                img(src='@/assets/curso/tema4/img3.svg', style="width:120px" alt='').m-auto
+            .col
+              p.mb-0 Las entidades responsables del pago de los servicios de salud deben difundir entre sus usuarios la conformación de su red de prestación de servicios. Para ello, deberán publicar anualmente en un periódico de amplia circulación en su área de influencia, el listado actualizado de los prestadores de servicios de salud que la conforman. Este listado debe estar organizado por el tipo de servicios contratados y por el nivel de complejidad.
 
 </template>
 
