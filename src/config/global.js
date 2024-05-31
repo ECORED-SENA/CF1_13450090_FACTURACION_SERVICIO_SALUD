8a6deefa6785dec8864bf0300fd43612e9ad01b5
emilydev <98632409+emilydev@users.noreply.github.com>
2024-05-31 17:03:53 -0500

    se vincula pdf y se genera descargable

--- src/config/global.js
+++ src/config/global.js
@@ -110,7 +110,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/13450090_CF1_DU.pdf',
       },
       {
         icono: 'fas fa-download',
